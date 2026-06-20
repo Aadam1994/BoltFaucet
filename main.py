9c0130cfcab7483bb81359716e0373e7517b43f3
Aadam1994 <mohamed.chb.94@gmail.com>
2026-06-20 15:35:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+os.environ["REPLIT_DB_URL"] = ""
+
 def main():
-    print("Hello from repl-nix-workspace!")
-
-
+    print("Hello from BoltFaucet")
+    
 if __name__ == "__main__":
     main()
